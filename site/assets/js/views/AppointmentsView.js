da584096f7d11ef53b43ef521a5ae413bcecbe9e
isemko <mystikum@gmail.com>
2013-11-22 19:40:09 -0500

    nav

--- site/assets/js/views/AppointmentsView.js
+++ site/assets/js/views/AppointmentsView.js
@@ -5,10 +5,10 @@ define(["lib/backbone", "views/EditView", "views/HeaderView", "collections/Appoi
 		subViews : [],
 		events : {
 			'click .edit' : 'editView'
-			
+
 		},
 		initialize : function(e) {
-
+		vent.on('dateheadercheck', this.checkHeader, this);
 			this.collection = new Collection();
 			// sort by date ascending
 			this.collection.comparator = function(m) {
@@ -30,49 +30,71 @@ define(["lib/backbone", "views/EditView", "views/HeaderView", "collections/Appoi
 			return $.format.date(new Date(d), 'ddd, MMMM d, yyyy').toUpperCase()
 		},
 		render : function() {
-		var self = this;
-		var temp_model = this.collection.first();
-		var temp_date = new Date(temp_model.get('startTime'));
-		var hView = new HeaderView({
-			temp_date : temp_date,
-			id : temp_model.get('_id'),
-			classId : temp_model.get('_id')
-		});
-		self.$el.append(hView.el);
-		self.subViews.push(hView);
-
-		this.collection.forEach(function(model) {
-			if (new Date(model.get('startTime')).getDay() != new Date(temp_date).getDay()) {
-				temp_model = model;
-				temp_date = temp_model.get('startTime');
-				var hView = new HeaderView({
-					temp_date : temp_date,
-					id : temp_model.get('_id'),
-					classId : temp_model.get('_id')
-				});
-				self.$el.append(hView.el);
-				self.subViews.push(hView);
-			}
-			var cView = new EditAppointmentView({
-				model : model,
-				headId : model.get('_id'),
+			var self = this;
+			var temp_model = this.collection.first();
+			var temp_date = new Date(temp_model.get('startTime'));
+			var hView = new HeaderView({
+				temp_date : temp_date,
+				id : temp_model.get('_id'),
 				classId : temp_model.get('_id')
 			});
-			self.subViews.push(cView);
-			self.$el.append(cView.el);
+			self.$el.append(hView.el);
+			self.subViews.push(hView);
 
-		})
+			this.collection.forEach(function(model) {
+				if (new Date(model.get('startTime')).getDay() != new Date(temp_date).getDay()) {
+					temp_model = model;
+					temp_date = temp_model.get('startTime');
+					var hView = new HeaderView({
+						temp_date : temp_date,
+						id : temp_model.get('_id'),
+						classId : temp_model.get('_id')
+					});
+					self.$el.append(hView.el);
+					self.subViews.push(hView);
+				}
+				var cView = new EditAppointmentView({
+					model : model,
+					headId : model.get('_id'),
+					classId : temp_model.get('_id')
+				});
+				self.subViews.push(cView);
+				self.$el.append(cView.el);
 
-			/*this.$el.prepend("<header>" + "<nav><ul id='main-nav'><li><a  class='edit'>Edit</a></li><li><a href='#home'>My Appointments</a></li>" + "<li  class='nav-last'><div class='add-main-inner'><a href='#add' class='add-main'>+</a></div></li></ul>" + "</nav></header>");
-		*/	
-	//this.$el.prepend("<header>" + "<nav><ul id='main-nav'><li><a href='#home'>Done</a></li><li><a href='#edit'>Edit Appointments</a></li>" + "<li  class='nav-last'></li></ul>" + "</nav></header>");
+			})
+		},
+	
+		checkHeader : function(e) {
+			//find total number of the day's class
+			$listCount = $('.' + $(e.$el[0]).attr('class').split(' ')[0]).length - 1;
+			// if only one, it is the heeader
+			if ($listCount == 1) {
+				//search views remove the header from subviews
+				for (var i = this.subViews.length - 1; i >= 0; i--) {
+					var check = (this.subViews[i].id ? this.subViews[i].id : this.subViews[i].model.id )
+					if (check == $(e.$el[0]).attr('class').split(' ')[0]) {
+						var x = this.subViews.splice(i, 1);
+						x[0].remove();
+					}
+				}
+			} else {
+				// if it is not the header, remove based on id in removeOneSubview
+				this.removeOneSubview(e);
+			}
 
 		},
-		editView: function(){
-		
+		removeOneSubview : function(e) {
+			for (var i = this.subViews.length - 1; i >= 0; i--) {
+				if (this.subViews[i].model.id == e.model.id) {
+					if (!e.$el.find('.date-head')) {
+						var x = this.subViews.splice(i, 1);
+						x[0].remove();
+					}
 
-		}
-,
+				}
+			}
+
+		},
 		close : function() {
 			while (this.subViews.length) {
 				var x = this.subViews.pop();
@@ -91,11 +113,11 @@ define(["lib/backbone", "views/EditView", "views/HeaderView", "collections/Appoi
 			// ???
 			this.$el = null;
 			// ???
-	
+
 			vent.off('dateheadercheck', this.checkHeader, this);
-			
+
 		}
 	});
-return AppointmentsView;
+	return AppointmentsView;
 });
 
--- site/assets/js/views/EditView.js
+++ site/assets/js/views/EditView.js
@@ -1,45 +1,45 @@
-define(["lib/backbone","lib/underscore", "views/AppointmentView", 'lib/text!templates/edit.html'], function(Backbone, _,  AppointmentView, editTemplate){
-	
-var EditAppointmentView = AppointmentView.extend({
-	tagName : 'li',
-	className : 'edit-list-item',
-	template : _.template(editTemplate),
-	initialize : function(options) {
-		vent.on('show-edit', this.showEdit, this);
+define(["lib/backbone", "lib/underscore", "views/AppointmentView", 'lib/text!templates/edit.html'], function(Backbone, _, AppointmentView, editTemplate) {
 
-		this.$el.attr("class", options.classId + ' ' + this.className);
+	var EditAppointmentView = AppointmentView.extend({
+		tagName : 'li',
+		className : 'edit-list-item',
+		template : _.template(editTemplate),
+		initialize : function(options) {
+			vent.on('show-edit', this.showEdit, this);
 
-		this.render();
+			this.$el.attr("class", options.classId + ' ' + this.className);
 
-	},
-	events : {
-		'click .del' : 'showDelete',
-		'click .hidden-delete-show' : 'removeAll'
-	},
-	removeAll : function() {
-		vent.trigger('dateheadercheck', this);
-		this.$el.empty();
-		this.model.destroy();
-		this.remove();
+			this.render();
 
-		return this;
-	},
-	showDelete : function() {
-		this.$el.find('.hidden-delete').addClass('hidden-delete-show').removeClass('hidden-delete');
-		this.$el.find('.fl').addClass('slide-left');
+		},
+		events : {
+			'click .del' : 'showDelete',
+			'click .hidden-delete-show' : 'removeAll'
+		},
+		removeAll : function() {
+			vent.trigger('dateheadercheck', this);
+			vent.off('show-edit', this.showEdit, this);
+			this.$el.empty();
+			this.model.destroy();
+			this.remove();
+			
+			return this;
+		},
+		showDelete : function() {
+			this.$el.find('.hidden-delete').addClass('hidden-delete-show').removeClass('hidden-delete');
+			this.$el.find('.fl').addClass('slide-left');
 
-	},
-	showEdit : function(){
-		if(this.$el.find('.app-link').attr('display') == 'none'){
-			this.$el.find('.app-link, .del').show();
+		},
+		showEdit : function() {
+			if (this.$el.find('.app-link').is(':visible')) {
+				this.$el.find('.app-link, .del').fadeOut('fast');
+			} else {
+				this.$el.find('.app-link, .del').fadeIn('fast');
+			}
+
 		}
-		else{
-			this.$el.find('.app-link, .del').hide();
-		}
-		
-	}
-});
-	
+	});
+
 	return EditAppointmentView;
-	
+
 });
