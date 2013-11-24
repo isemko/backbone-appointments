b879a5f613832b0825f5b7d1f37e6db6f0c9a8e2
isemko <mystikum@gmail.com>
2013-11-24 11:16:18 -0500

    fixed

--- site/assets/js/views/AppointmentsView.js
+++ site/assets/js/views/AppointmentsView.js
@@ -68,7 +68,7 @@ define(["lib/backbone", "views/EditView", "views/HeaderView", "collections/Appoi
 
 					$('#main-data').attr('class', 'center transition');
 					$('#alt-data').attr('class', 'right transition');
-					$('#content').css('min-height', $('#main-data').height())
+					$('#content').css('height', $('#main-data').height() +5+'px');
 				}
 		},
 
